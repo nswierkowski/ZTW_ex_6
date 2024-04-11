36bef303b5802e58e7031191b4d1b404f84a061a
nswierkowski <nikodem.swierkowski@gmail.com>
2024-04-11 08:37:31 +0200

    add forgotten layouts

---
+++ address-book-vue-app/src/components/layouts/DefaultLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header>
+      <h1>Witaj na stronie developerskiej</h1>
+    </header>
+
+    <main>
+      <slot></slot>
+    </main>
+
+    <footer>
+      <p>Wybierz zakładkę</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+}
+
+header h1{
+  color: white;
+}
+
+footer {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+}
+</style>
---
+++ address-book-vue-app/src/components/layouts/LibraryLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <h1>Witaj na stronie czytelni</h1>
+    </header>
+
+    <main>
+      <slot></slot>
+    </main>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlternativeLayout',
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #ff9800;
+  color: white;
+  padding: 10px;
+}
+footer {
+  background-color: #ff9800;
+  color: white;
+  padding: 10px;
+}
+</style>
